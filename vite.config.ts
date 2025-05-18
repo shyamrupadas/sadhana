a72af606ab713cfe2f3b15299d3eb9a5ad9f1d82
Sergey Maksimov <s.mksimov@gmail.com>
2025-05-18 12:04:07 +0300

    Add PWA

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,43 @@ import path from 'path'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    basicSsl(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      manifest: {
+        name: 'SadhanaTracker',
+        short_name: 'Sadhana',
+        start_url: '/',
+        description: 'Трекер сна и привычек',
+        display: 'standalone',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: '/check-192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/check-512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
+  preview: {
+    host: true,
+    https: {},
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
