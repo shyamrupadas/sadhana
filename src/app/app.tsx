d4005eb17cc17b88da20719383eba0065ec7b777
Sergey Maksimov <s.mksimov@gmail.com>
2026-01-17 12:30:51 +0700

    Fix vertical scroll for PWA

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -4,7 +4,7 @@ import { Providers } from './providers'
 export const App = () => {
   return (
     <Providers>
-      <div className="min-h-screen flex flex-col">
+      <div className="min-h-[100svh] min-h-[100dvh] flex flex-col">
         <Outlet />
       </div>
     </Providers>
